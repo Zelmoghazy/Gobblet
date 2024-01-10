80d08077ce6c4eca09520cdac2d7bbb63345c305
Ahmed Hassan Ibrahim <Ahmed Hassan Ibrahim>
2024-01-10 21:15:23 +0200

    GUI Alpha code mostly 'human vs human' functionality

---
+++ GUI/GUI_Alpha.py
@@ -0,0 +1,238 @@
+import pygame
+import sys
+import math
+from collections import defaultdict
+
+pygame.init()
+
+# Colors
+black  = (0, 0, 0)
+white  = (255, 255, 255)
+
+# Positions
+WIDTH, HEIGHT = 800, 800
+LEFT_MARGIN = 200
+RIGHT_MARGIN = 200
+CONTROL_PANEL = 300
+
+# Grid
+GRID_SIZE = 4
+CELL_SIZE = WIDTH // GRID_SIZE
+LINE_WIDTH = 10
+
+#Create Screen
+screen = pygame.display.set_mode((LEFT_MARGIN+WIDTH+RIGHT_MARGIN+CONTROL_PANEL,HEIGHT))
+pygame.display.set_caption("Gobblet")
+icon = pygame.image.load("Images\\Icon.png")
+pygame.display.set_icon(icon)
+
+#Add Background Image
+Background_Img = pygame.image.load("Images//Board.png")
+Background_Img = pygame.transform.scale(Background_Img, (WIDTH + LEFT_MARGIN*2 + CONTROL_PANEL, HEIGHT))
+
+# Center to snap pieces
+centers = []
+
+# Pieces
+circles = []
+circle_map = defaultdict(list)
+
+active_circle = None
+active_circle_old_x = None
+active_circle_old_y = None
+active_player='white'
+
+# Text
+font = pygame.font.Font(None, 40)
+text_box = font.render("Text Box", True, 'white')   
+text = font.render("Illegal Move !!!!", True, 'red') 
+white_turn = font.render("White Player Turn", True, 'red') 
+black_turn = font.render("Black Player Turn", True, 'red')
+white_win = font.render("White Wins!", True, 'green')
+black_win = font.render("Black Wins!", True, 'green')
+w_turn = font.render("White turn", True, 'white')
+b_turn = font.render("black turn", True, 'white')
+
+# Restart Button
+restart_button_rect = pygame.Rect(WIDTH + RIGHT_MARGIN * 2 + 50, HEIGHT - HEIGHT + 50, 200, 50)
+restart_button_color = (255, 165, 0) 
+restart_button_text = font.render("Restart Game", True, 'black')
+ 
+#Cirlce Class============================================================
+class Circle:
+    def __init__(self, color, center, radius,num):
+        self.num = num    #id
+        self.color = color
+        self.rect = pygame.Rect(center[0] - radius, center[1] - radius, 2 * radius, 2 * radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen,self.color,self.rect.center,self.rect.width)
+
+    def __eq__(self, other):
+        if isinstance(other, Circle):
+            return self.num == other.num
+        return False
+
+#Class for Game Initialization========================================== 
+class Game:
+    def __init__(self) -> None: # Constructor
+        self.guide_lines()
+        self.winner = None
+
+    def guide_lines(self):
+        pos = 0
+        for i in range(GRID_SIZE):
+            for j in range(GRID_SIZE):
+                center_x = i * CELL_SIZE + LEFT_MARGIN + CELL_SIZE // 2
+                center_y = j * CELL_SIZE + CELL_SIZE // 2
+                centers.append((center_x,center_y,pos))
+                pos += 1
+        num = 0
+
+        for i in range(GRID_SIZE-1):
+            for j in range(GRID_SIZE):
+                white_circle = Circle("white", (GRID_SIZE * CELL_SIZE + LEFT_MARGIN + CELL_SIZE // 2, i * CELL_SIZE + CELL_SIZE), (j + 1) * 10,num)
+                circles.append(white_circle)
+                num += 1
+                black_circle = Circle("black", (RIGHT_MARGIN // 2, i * CELL_SIZE + CELL_SIZE), (j + 1) * 10,num)
+                circles.append(black_circle)
+                num += 1
+        circles.sort(key=lambda circle: circle.rect.width)
+        for idx, circle in enumerate(circles):
+            circle.num = idx
+            idx+=1
+        # Winning combinations
+        self.winning_combinations = []
+        for i in range(GRID_SIZE):
+            # Horizontal
+            self.winning_combinations.append([(i * CELL_SIZE + LEFT_MARGIN + CELL_SIZE // 2, j * CELL_SIZE + CELL_SIZE // 2) for j in range(GRID_SIZE)])
+            # Vertical
+            self.winning_combinations.append([(j * CELL_SIZE + LEFT_MARGIN + CELL_SIZE // 2, i * CELL_SIZE + CELL_SIZE // 2) for j in range(GRID_SIZE)])
+        # Diagonal from top-left to bottom-right
+        self.winning_combinations.append([(j * CELL_SIZE + LEFT_MARGIN + CELL_SIZE // 2, j * CELL_SIZE + CELL_SIZE // 2) for j in range(GRID_SIZE)])
+        # Diagonal from top-right to bottom-left
+        self.winning_combinations.append([(j * CELL_SIZE + LEFT_MARGIN + CELL_SIZE // 2, (GRID_SIZE - 1 - j) * CELL_SIZE + CELL_SIZE // 2) for j in range(GRID_SIZE)])
+
+def main():
+    game = Game() 
+    illegal = False
+    game_over = False
+    while True:
+        global active_circle
+        global active_player
+        
+        #Change Background Image
+        screen.blit(Background_Img, (0, 0))
+
+        #Add Text Box Title
+        screen.blit(text_box, (WIDTH + RIGHT_MARGIN*2 + 10, HEIGHT - 270))
+
+        # Draw Restart Button
+        pygame.draw.rect(screen, restart_button_color, restart_button_rect)
+        screen.blit(restart_button_text, (restart_button_rect.x + 10, restart_button_rect.y + 10))
+
+        #Display Player Turn
+        if active_player == 'white':
+            screen.blit(w_turn, (WIDTH + RIGHT_MARGIN*2 + 20, HEIGHT - 400))
+        if active_player == 'black':
+            screen.blit(b_turn, (WIDTH + RIGHT_MARGIN*2 + 20, HEIGHT - 400))
+
+        # Draw all circles
+        for circle in circles:
+            circle.draw(screen)
+
+       # Illegal Play
+        if illegal:
+            screen.blit(text, (WIDTH + RIGHT_MARGIN*2 + 20, HEIGHT - 200))
+            if(active_player == 'white'):
+                screen.blit(white_turn,(WIDTH + RIGHT_MARGIN*2 + 20, HEIGHT - 150))
+            else:
+                screen.blit(black_turn,(WIDTH + RIGHT_MARGIN*2 + 20, HEIGHT - 150))
+
+        #Check for winner
+        if not illegal:
+            for combo in game.winning_combinations:
+                color_count = {'white': 0, 'black': 0}
+                for center in combo:
+                    if circle_map[center]:
+                        color_count[circle_map[center][-1].color] += 1
+                if color_count['white'] == GRID_SIZE:
+                    screen.blit(white_win, (WIDTH + RIGHT_MARGIN*2 + 20, HEIGHT - 200))
+                    game_over = True
+                    
+                elif color_count['black'] == GRID_SIZE:
+                    screen.blit(black_win, (WIDTH + RIGHT_MARGIN*2 + 20, HEIGHT - 200))
+                    game_over = True
+                    
+
+        # Events
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if restart_button_rect.collidepoint(mouse_pos):
+                        # Restart the game
+                        circle_map.clear()
+                        circles.clear()
+                        illegal = False
+                        game_over = False
+                        active_player = 'white'
+                        game = Game()             
+                        continue
+                    
+                    for circle in circles:
+                        if circle.rect.collidepoint(event.pos) and game_over == False:
+                            top_circle = circle
+                            if(circle_map[top_circle.rect.center]):
+                                top_circle = circle_map[top_circle.rect.center][-1]
+
+                            if(top_circle.color != active_player):
+                                illegal=True
+                                break
+                            active_circle_old_x = top_circle.rect.centerx
+                            active_circle_old_y = top_circle.rect.centery
+                            active_circle = top_circle.num
+
+            if event.type == pygame.MOUSEMOTION:
+                if active_circle is not None:
+                    circles[active_circle].rect.move_ip(event.rel)
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1 and active_circle is not None:
+                    # Find the nearest center
+                    dragged_rect = circles[active_circle].rect
+                    nearest_center = min(centers, key=lambda center: math.dist((dragged_rect.centerx, dragged_rect.centery), (center[0],center[1])))
+                    # Compare dragged and placed pieces
+                    overlapping_circle = next((circle for circle in circles if circle.rect.centerx == nearest_center[0] and circle.rect.centery == nearest_center[1] and circle.rect.width >= dragged_rect.width), None)
+
+                    if overlapping_circle is not None:
+                        # Revert Old Positions
+                        dragged_rect.centerx = active_circle_old_x
+                        dragged_rect.centery = active_circle_old_y
+                    else:                        
+                        # Snap the dragged rectangle to the nearest center
+                        dragged_rect.centerx = nearest_center[0]
+                        dragged_rect.centery = nearest_center[1]
+
+                        # Update board
+                        circle_map[(nearest_center[0],nearest_center[1])].append(circles[active_circle])
+
+                        if circle_map[(active_circle_old_x,active_circle_old_y)]:
+                            circle_map[(active_circle_old_x,active_circle_old_y)].pop()
+
+                        illegal = False
+                        # Switch Active Player
+                        if active_player == 'white':
+                            active_player = 'black'
+                        else:
+                            active_player = 'white'
+
+                    active_circle = None
+        
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+                
+        pygame.display.flip()
+
+main()
